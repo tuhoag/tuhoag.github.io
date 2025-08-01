c5c69df176bf2ce3a578cd5c6fd06c0dac0f59f0
tuhoag <hatu87@gmail.com>
2025-08-01 15:49:07 +0200

    add vercel analytic

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import { Outlet } from "react-router";
 
 import Header from '../components/Header'
@@ -16,6 +17,7 @@ function RootLayout() {
       <Header contact={contact} links={links} />
       <Outlet />
       <Footer contact={contact} links={links} accounts={accounts} researchInterests={researchInterests} />
+      <Analytics />
     </div>
   )
 }
